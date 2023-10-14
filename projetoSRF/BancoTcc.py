58b431e9b8b926fd56dac336f4b79cce4e94a484
João Vitor de Sousa Rodrigues <99426556+JoaoVitordSRodrigues@users.noreply.github.com>
2023-10-14 01:32:31 -0300

    versao classe dividida
    versao com telas com respectivas classes

---
+++ projetoSRF/BancoTcc.py
@@ -0,0 +1,16 @@
+import sqlite3 
+
+conn = sqlite3.connect('Scanner.db')
+
+cursor = conn.cursor()
+
+#cursor.execute("""
+#CREATE TABLE IF NOT EXISTS cadastroAlu (
+ #   nome TEXT, 
+  #  rm INTEGER,
+   # checbox_periodo INTEGER, 
+    #checkbox_curso  INTEGER,
+    #checkbox_turma  INTEGER,
+    #fotoAlu BLOB
+#);
+#""")
